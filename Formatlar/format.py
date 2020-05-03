05ff30d27492846bbedddc662410fe0c653c9db4
burakmt <burakekici44@gmail.com>
2020-05-03 18:57:02 +0300

    Formatlar

---
+++ Formatlar/format.py
@@ -0,0 +1,10 @@
+name = "Burak"
+surname = "Ekici"
+
+# print('Test {0} {1}'.format(name,surname))
+
+# FORMAT İÇİ DEĞİŞKEN ATAMALARI YAPABİLİYORUZ
+print('Test {n} {s}'.format(n = name, s = surname))
+
+#STRING İFADENİN İÇİNE DEĞİŞKEN ATAMA
+print(f"Test {name} {surname}")
